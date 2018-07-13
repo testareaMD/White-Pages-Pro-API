ad6f4164cb2ec6d256f5aa133959790b84cd369f
Robert Edwardes <rdedwardes@gmail.com>
2018-07-13 09:42:46 -0400

    for each fix

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ const nameZip = [
     "Zip": 21128,
     "StateVoterID": "NULL"
   }];
-function paperSurvey(array){
-    let firstName = array.firstName;
-    let lastName = array.lastName;
-    let zip = array.Zip;
-    var i;
-    var url_string = "https://proapi.whitepages.com/3.0/person?name=" + firstName + '+' + lastName + "&address.postalcode=" + zip + "&address.state.code=MD&api_key=APIKEY";
-    console.log(url_string);
-  }
-nameZip.ForEach(paperSurvey);
+  nameZip.forEach(function paperSurvey(array){
+      let firstName = array.firstName;
+      let lastName = array.lastName;
+      let zip = array.Zip;
+      var i;
+      let url_string = "https://proapi.whitepages.com/3.0/person?name=" + firstName + '+' + lastName + "&address.postalcode=" + zip + "&address.state.code=MD&api_key=APIKEY";
+      console.log(url_string);
+    });
+paperSurvey(nameZip);
