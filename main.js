7608398bedb9552b43a9b6e1f602f0fe329c2599
Robert Edwardes <rdedwardes@gmail.com>
2018-07-13 13:14:24 -0400

    added check
    no api key yet

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const nameZip = [
       let firstName = res.firstName;
       let lastName = res.lastName;
       let zip = res.Zip;
+      console.log('https://proapi.whitepages.com/3.0/person?name=" + firstName + '+' + lastName + "&address.postalcode=" + zip + "&address.state.code=MD&api_key=APIKEY')
       this.get_url = function(){
         $.getJSON("https://proapi.whitepages.com/3.0/person?name=" + firstName + '+' + lastName + "&address.postalcode=" + zip + "&address.state.code=MD&api_key=APIKEY", function(person){
           let street1 = person.found_at_address.street_line_1;
@@ -24,6 +25,7 @@ const nameZip = [
           let zip = person.found_at_address.zip4;
           let name = person.name;
           $('#results').append('<tr><td>' + name + '</td><td>' + street1 + '</td><td>' + street2 + '</td><td>' + city + '</td><td>' + zip + '</td></tr>');
+          console.log(done);
         });
       };
     });
